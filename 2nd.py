e4e6c8077264857c89c46d5851f966cee365e31a
1210deepak <75420999+1210deepak@users.noreply.github.com>
2020-12-04 21:28:03 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,6 @@
+#simple interest
+p = int(input("enter the principle amount"))
+t = int(input("Enter the time period"))
+rate = int(input("Enter the rate of interest"))
+si=(p*t*rate)/100
+print("the simple intrest is ",si)
